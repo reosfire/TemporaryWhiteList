f89085255c89e564dd1b42e06210c5584d313ec1
reosfire <2.com9761721@gmail.com>
2022-02-12 21:03:59 +0300

    PlayerLoginEvent -> AsyncPlayerPreLoginEvent

--- src/main/java/ru/reosfire/temporarywhitelist/EventsListener.java
+++ src/main/java/ru/reosfire/temporarywhitelist/EventsListener.java
@@ -1,9 +1,11 @@
 package ru.reosfire.temporarywhitelist;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import ru.reosfire.temporarywhitelist.Configuration.Config;
 import ru.reosfire.temporarywhitelist.Data.IDataProvider;
 import ru.reosfire.temporarywhitelist.Lib.Text.Text;
@@ -20,16 +22,17 @@ public class EventsListener implements Listener
         PluginInstance = pluginInstance;
     }
 
-    @EventHandler
-    public void onJoin(PlayerLoginEvent event)
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(AsyncPlayerPreLoginEvent event)
     {
         if(!PluginInstance.isWhiteListEnabled()) return;
-        Player player = event.getPlayer();
-        if (!DataProvider.CanJoin(player.getDisplayName()))
+        OfflinePlayer player = Bukkit.getOfflinePlayer(event.getUniqueId());
+
+        if (!DataProvider.CanJoin(player.getName()))
         {
             if (!player.isOp())
             {
-                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, Text.Colorize(player, Configuration.Messages.KickOnConnecting));
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, Text.Colorize(player, Configuration.Messages.KickOnConnecting));
             }
         }
     }
