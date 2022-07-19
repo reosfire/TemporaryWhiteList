114276900730aa1a9924aba4b4ce98a543663e05
reosfire <2.com9761721@gmail.com>
2022-07-19 03:58:44 +0300

    bstats classes from maven

--- src/main/java/ru/reosfire/temporarywhitelist/TemporaryWhiteList.java
+++ src/main/java/ru/reosfire/temporarywhitelist/TemporaryWhiteList.java
@@ -1,5 +1,8 @@
 package ru.reosfire.temporarywhitelist;
 
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
+import org.bstats.charts.SingleLineChart;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
@@ -43,8 +46,8 @@ public final class TemporaryWhiteList extends JavaPlugin
     {
         Load();
         Metrics metrics = new Metrics(this, 14858);
-        metrics.addCustomChart(new Metrics.SingleLineChart("whitelisted_players", () -> _database.AllList().size()));
-        metrics.addCustomChart(new Metrics.SimplePie("whitelisted_players_per_server", () -> Integer.toString(_database.AllList().size())));
+        metrics.addCustomChart(new SingleLineChart("whitelisted_players", () -> _database.AllList().size()));
+        metrics.addCustomChart(new SimplePie("whitelisted_players_per_server", () -> Integer.toString(_database.AllList().size())));
     }
 
     public void Load()
