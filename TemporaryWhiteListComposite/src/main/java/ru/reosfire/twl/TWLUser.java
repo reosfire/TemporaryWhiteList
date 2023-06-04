7b693fea47282ce0142a9fc26618fc093b4f808e
reosfire <2.com9761721@gmail.com>
2023-06-04 22:15:27 +0300

    jar minimizing

---
+++ TemporaryWhiteListComposite/src/main/java/ru/reosfire/twl/TWLUser.java
@@ -0,0 +1,9 @@
+package ru.reosfire.twl;
+
+public class TWLUser {
+
+    @SuppressWarnings("unused")
+    public void use() {
+        TemporaryWhiteList temporaryWhiteList = new TemporaryWhiteList();
+    }
+}
