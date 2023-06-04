e28897cebbe40af78e2741f82d49be60318ab29c
reosfire <2.com9761721@gmail.com>
2023-06-04 23:44:04 +0300

    commented this strange thing

--- TemporaryWhiteListComposite/src/main/java/ru/reosfire/twl/TWLUser.java
+++ TemporaryWhiteListComposite/src/main/java/ru/reosfire/twl/TWLUser.java
@@ -3,6 +3,7 @@ package ru.reosfire.twl;
 public class TWLUser {
 
     @SuppressWarnings("unused")
+    //This thing need to make shade plugin not delete everything on optimize jar
     public void use() {
         ru.reosfire.twl.spigot.TemporaryWhiteList spigot = new ru.reosfire.twl.spigot.TemporaryWhiteList();
         ru.reosfire.twl.velocity.TemporaryWhiteList velocity = new ru.reosfire.twl.velocity.TemporaryWhiteList();
